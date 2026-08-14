60bc84db096090de8ba1fb28fc38ee55b865c958
lvieira <vieira.luc@hotmail.com>
2026-08-13 21:32:08 -0300

    Substituindo a prática03

---
+++ praticas/pratica03/app-atividade-aula-tarefas/App.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { rotulo_input_meta, rotulo_btn_cadastro_meta, rotulo_lista_metas } from './mensagens';
+
+export default function App() {
+  return (
+    <View style={styles.mainContainer}>
+      <TextInput placeholder={rotulo_input_meta}/>
+      <Button title={rotulo_btn_cadastro_meta} />
+      <Text>{rotulo_lista_metas}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainContainer: {
+    padding: 30,
+  },
+});
