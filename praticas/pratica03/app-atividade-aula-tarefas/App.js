bf805eb6c3c16639a260a790874e1ebcbfbdb3f1
unknown <2424290016@ACADEMICO.IESB.BR>
2026-08-20 21:45:42 -0300

    aaaaaa

--- praticas/pratica03/app-atividade-aula-tarefas/App.js
+++ praticas/pratica03/app-atividade-aula-tarefas/App.js
@@ -1,24 +1,44 @@
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
-import { rotulo_input_meta, rotulo_btn_cadastro_meta, rotulo_lista_metas } from './mensagens';
+import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import MetasList from "./components/MetasList";
+import MetaInput from "./components/MetaInput";
 
 export default function App() {
+  const [metas, setMetas] = useState([]);
+
+  function adicionarMetaHandler() {
+    setMetas([...metas, inputMetaText]);
+  }
+
   return (
     <View style={styles.mainContainer}>
-      <TextInput placeholder={rotulo_input_meta}/>
-      <Button title={rotulo_btn_cadastro_meta} />
-      <Text>{rotulo_lista_metas}</Text>
+      <MetaInput onAddMeta={adicionarMetaHandler} />
+
+      <View style={styles.metaContainer}>
+        <MetasList array={metas} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   mainContainer: {
+    marginTop: 20,
     padding: 30,
+    flex: 1,
+    flexDirection: "column",
+  },
+  metaContainer: {
+    flex: 15,
+  },
+  item: {
+    margin: 8,
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: "lightblue",
   },
 });
