10125ed6f80b919e23bf900c3fa7b997f1dcdf9a
unknown <2424290016@ACADEMICO.IESB.BR>
2026-09-10 21:28:29 -0300

    Estilizacao inicial

--- praticas/pratica06/RotinaIESB/App.js
+++ praticas/pratica06/RotinaIESB/App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"; //provavelmente não vai precisar usar, excluir se for o caso
+import { StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { CompromissoInput } from "./components/CompromissoInput";
+import { tituloApp } from "./labels";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.screen}>
+        <View style={styles.header}>
+          <Image
+            style={styles.headerImage}
+            source={require("./assets/logo.png")}
+          />
+          <Text style={styles.headerText}>{tituloApp}</Text>
+        </View>
+        <View style={styles.main}>
+          <View style={styles.input}>{CompromissoInput}</View>
+          <View style={styles.list}></View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  headerImage: {
+    margin: 32,
+    width: 64,
+    height: 64,
+  },
+  headerText: {
+    marginLeft: "auto",
+    marginRight: 85,
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "red",
+  },
+  main: {
+    flex: 6.5,
+    backgroundColor: "yellow",
+  },
+  input: {
+    backgroundColor: "green",
+  },
+  list: {
+    backgroundColor: "blue",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ praticas/pratica06/RotinaIESB/labels.js
@@ -0,0 +1,7 @@
+const tituloApp = "Rotina IESB";
+const placeholderCompromisso = "Placeholder Compromisso";
+const botaoAdicionar = "Adicionar";
+const tituloLista = "Lista";
+const listaVazia = "Adicione um compromisso";
+
+export { tituloApp, placeholderCompromisso, botaoAdicionar, tituloLista, listaVazia};
